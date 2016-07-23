7cf04b51f0c0cbdf18994717f0e0eebe0688ed25
J45k4 <pusanteemu@gmail.com>
2016-07-23 17:26:15 +0300

    Remove useles foreign key relation.

--- matti/sql/profile_table.sql
+++ matti/sql/profile_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE profiles (
 	id INT NOT NULL AUTO_INCREMENT,
-	name INT NOT NULL,
+	name VARCHAR(50) NOT NULL,
 	token INT NOT NULL,
 	PRIMARY KEY(id),
-	FOREIGN KEY(name) REFERENCES names(id),
 	FOREIGN KEY(token) REFERENCES token(id)
 );
