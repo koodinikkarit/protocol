e97d5f1e883922324b4c75740e2db4ca3baf35a1
Jaska <ei.kuulu@sulle.t>
2016-12-13 19:16:27 +0200

    Päivitetty matin proto tiedostoja.

--- matti/build.bat
+++ matti/build.bat
@@ -1,10 +1,13 @@
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python AllConnections.proto
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python KwmConnection.proto
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python KwmConnections.proto
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python MatrixProto.proto
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python MatrixProtos.proto
 protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python MattiRequest.proto
 protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python MattiResponse.proto
-
-protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python RequestValue.proto
-
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python ProgramProto.proto
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python ProgramProtos.proto
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python TimerProto.proto
+protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python TimerProtos.proto
 protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python VideoConnection.proto
-protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python KwmConnection.proto
-
 protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python VideoConnections.proto
-protoc --csharp_out=csharp --cpp_out=cpp --objc_out=objc --java_out=java --python_out=python KwmConnections.proto
