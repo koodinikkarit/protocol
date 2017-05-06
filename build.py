1df4062c7d508d4047b4d41aa0a33bbb2bf0e6f3
Jaska <pusanteemu@gmail.com>
2017-05-06 18:02:27 +0300

    Poistettu nodesta proto tiedostojen siirto.

--- build.py
+++ build.py
@@ -29,7 +29,7 @@ with open("config.json") as json_file:
 				files = os.listdir(project["service"])
 				for file in files:
 					if file.endswith(".proto"):
-						shutil.copy(project["service"] + "/" + file, output["path"] + "/" + file)
+						#shutil.copy(project["service"] + "/" + file, output["path"] + "/" + file)
 						os.system("grpc_tools_node_protoc --proto_path=" + project["service"] + " --js_out=import_style=commonjs,binary:" + output["path"] + " --grpc_out=" + output["path"] + " --plugin=grpc_tools_node_protoc_plugin " + project["service"] + "/" + file)
 
 
